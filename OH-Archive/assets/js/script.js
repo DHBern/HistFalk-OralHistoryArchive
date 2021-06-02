10b4e36496cfe06ea31b8e4eb50b9936d3119e0e
Nina Baumgartner <nina.baumgartner@students.unibe.ch>
2021-06-02 13:59:41 +0200

    Name Buttons are now displayes in index.html. No links yet.

--- OH-Archive/assets/js/script.js
+++ OH-Archive/assets/js/script.js
@@ -2,59 +2,73 @@
 ToDO:
 - Fix Geo error in objects: 3541, 3662, 3663
  */
-
-const request = new XMLHttpRequest();
+//Dom Elements
 const app = document.getElementById('root');
-const container = document.createElement('div');
-container.setAttribute('class', 'container');
-container.setAttribute("style", "padding:20px")
-app.appendChild(container);
+const flexContainer = document.createElement("ul");
+flexContainer.setAttribute("class","flex-container wrap" );
+app.appendChild(flexContainer);
+//Request to API
+const request = new XMLHttpRequest();
 request.open('GET', 'https://www.corona-memory.ch/api/items?per_page=999999&item_set_id=3527', true);
 request.withCredentials = false;
+
 request.onload = function () {
+
     // Begin accessing JSON data here
     var data = JSON.parse(this.response);
     if (request.status >= 200 && request.status < 400) {
         data.forEach((object) => {
-            const titleBtn = document.createElement('Button');
-            titleBtn.setAttribute('class', 'btn btn-primary');
-            titleBtn.setAttribute('type', 'Button');
+
+            //Dom Button
+            const nameBtn = document.createElement('Button');
+            nameBtn.setAttribute('class', 'btn btn-primary');
+            nameBtn.setAttribute('type', 'Button');
+            nameBtn.setAttribute("display","inline-block");
+
+            const flexItem = document.createElement("li");
+            flexItem.setAttribute("class", "flex-item");
+
             var obj_values = Object.values(object);
+
             /*
             Entry information
-            [id, is_public, title, created, lastModified, description, language]
              */
-            var entry_info =[obj_values[3], obj_values[4], obj_values[9],Object.values(obj_values[11])[0],
-                Object.values(obj_values[12])[0], Object.values(obj_values[17][0])[4],
-                Object.values(obj_values[24][0])[4]];
+            const Entry = {
+                id: BigInt(obj_values[3]),
+                isPublic: obj_values[4] ,
+                title: obj_values[9],
+                description: Object.values(obj_values[17][0])[4],
+                language:Object.values(obj_values[24][0])[4],
+                created: Object.values(obj_values[11])[0],
+                lastModified: Object.values(obj_values[12])[0]
+            };
             /*
-           Person information
-            [firstName, familyName, birthday]
-             */
-
-            var pers_info = [Object.values(obj_values[19][0])[4], Object.values(obj_values[20][0])[4],
-                Object.values(obj_values[18][0])[4]];
+         Person information
+           */
+            const Person = {
+                firstName: Object.values(obj_values[19][0])[4],
+                lastName: Object.values(obj_values[20][0])[4],
+                birthday: Object.values(obj_values[18][0])[4]
+            };
             /*
             Geo information
-            [hasGeoLocation, locatedIn, isSubjectOf]
              */
+            const GeoInfo ={
+                hasGeoLocation: Object.values(obj_values[21][0])[4],
+                locatedIn: Object.values(obj_values[22][0])[4] ,
+                isSubjectOf: Object.values(obj_values[23][0])[4]
+            };
 
-            var geo_info = [Object.values(obj_values[21][0])[4],Object.values(obj_values[22][0])[4],
-                Object.values(obj_values[23][0])[4]];
-
-            const title = document.createTextNode(entry_info[2]);
-            titleBtn.appendChild(title)
-            // Append the button to the container element
-            container.appendChild(titleBtn);
-
-
-
-
+            // Append text to button and button to the container element
+            const name = document.createTextNode(Person.firstName+" "+Person.lastName);
+            nameBtn.appendChild(name);
+            flexItem.appendChild(nameBtn);
+            flexContainer.appendChild(flexItem);
 
         })
     } else {
-        const errorMessage = document.createElement('marquee')
-        errorMessage.textContent = `Gah, it's not working!`
+        const errorMessage = document.createElement('marquee');
+        errorMessage.textContent = `Gah, it's not working!`;
         app.appendChild(errorMessage);
     }
 
